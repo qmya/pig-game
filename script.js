d698f20678d1d9a35e771429d4776626c6af1401
qmya <qandeelgul90@gmail.com>
2020-12-10 16:24:52 -0500

    added the funtionality to reset button

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const score2El = document.querySelector('#score--1');
 let diceEl = document.querySelector('.dice');
 const buttonRoll = document.querySelector('.btn--roll');
 const buttonHold = document.querySelector('.btn--hold');
+const buttonReset = document.querySelector('.btn--new');
 let CurrentPlayerScore1 = document.querySelector('#current--0');
 let CurrentPlayerScore2 = document.querySelector('#current--1');
 const player0El = document.querySelector('.player--0');
@@ -26,6 +27,7 @@ const randomDiceRoll = Math.trunc(Math.random() * 6) + 1;
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
 //switch players
 const switchPlayers = function () {
   currentScore = 0;
@@ -40,40 +42,65 @@ const switchPlayers = function () {
 };
 buttonRoll.addEventListener('click', function () {
   console.log('button is click');
-  //Genenating the Random number
-  //Display the Dice
-  const randomDiceRoll = Math.trunc(Math.random() * 6) + 1;
-  console.log(randomDiceRoll);
-  diceEl.classList.remove('hidden');
+  if (playing) {
+    //Genenating the Random number
+    //Display the Dice
+    const randomDiceRoll = Math.trunc(Math.random() * 6) + 1;
+    console.log(randomDiceRoll);
+    diceEl.classList.remove('hidden');
 
-  //display the random image according to the number
-  diceEl.src = `dice-${randomDiceRoll}.png`;
-  //Check if the dice is not equal to 1 then add the score else switch the players
-  if (randomDiceRoll !== 1) {
-    //Add the dice to the current player
-    currentScore += randomDiceRoll;
-    document.getElementById(
-      `current--${activePlayer}`
-    ).textContent = currentScore;
-    // CurrentPlayerScore1.textContent = currentScore;
-  } else {
-    //Switch the players
-    switchPlayers();
+    //display the random image according to the number
+    diceEl.src = `dice-${randomDiceRoll}.png`;
+    //Check if the dice is not equal to 1 then add the score else switch the players
+    if (randomDiceRoll !== 1) {
+      //Add the dice to the current player
+      currentScore += randomDiceRoll;
+      document.getElementById(
+        `current--${activePlayer}`
+      ).textContent = currentScore;
+      // CurrentPlayerScore1.textContent = currentScore;
+    } else {
+      //Switch the players
+      switchPlayers();
+    }
   }
 });
 
 buttonHold.addEventListener('click', function () {
-  //1.Add current score to the total
-  //2.if score >=100
-  //3.then current player wins
-  //4.else switch the players
-  scores[activePlayer] += currentScore;
-  document.getElementById(`score--${activePlayer}`).textContent =
-    scores[activePlayer];
-  console.log(`score--${activePlayer}.textContent = scores[activePlayer]`);
-  if (scores[activePlayer] >= 100) {
-    console.log(`current--${activePlayer} wins`);
-  } else {
-    switchPlayers();
+  if (playing) {
+    //1.Add current score to the total
+    //2.if score >=100
+    //3.then current player wins
+    //4.else switch the players
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    console.log(`score--${activePlayer}.textContent = scores[activePlayer]`);
+    if (scores[activePlayer] >= 10) {
+      playing = false;
+      console.log(`current--${activePlayer} wins`);
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayers();
+    }
   }
+});
+
+//reseting everything
+buttonReset.addEventListener('click', function () {
+  console.log('reset button clicked');
+  playing = true;
+  score1El.textContent = 0;
+  score2El.textContent = 0;
+  CurrentPlayerScore1.textContent = 0;
+  CurrentPlayerScore1.textContent = 1;
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
 });
